27dfd5fbce829ff01056f0a1f9e69c4000d082e6
tomigiani1 <tgiani@fi.uba.ar>
2024-09-20 08:57:58 -0300

    first commit

---
+++ My_First_Statechart_Export.scm
@@ -0,0 +1,298 @@
+{
+  "graph": {
+    "cells": [
+      {
+        "position": {
+          "x": 0,
+          "y": 0
+        },
+        "size": {
+          "height": 10,
+          "width": 10
+        },
+        "angle": 0,
+        "type": "Statechart",
+        "id": "78749915-0da0-40a2-862f-9e8d94c7c68e",
+        "linkable": false,
+        "z": 1,
+        "attrs": {
+          "name": {
+            "text": "My First Statechart Export"
+          },
+          "specification": {
+            "text": "@EventDriven\n@SuperSteps(no)\n\ninterface: \n    in event myEvent\n"
+          }
+        }
+      },
+      {
+        "type": "Entry",
+        "position": {
+          "x": 222.5,
+          "y": 130
+        },
+        "size": {
+          "height": 15,
+          "width": 15
+        },
+        "angle": 0,
+        "fixedRatio": true,
+        "embedable": false,
+        "linkable": true,
+        "id": "a4d257fe-72cb-4334-9a1f-b5a8b4f886fb",
+        "z": 2,
+        "embeds": [
+          "f70f632c-e54d-4002-9ec5-7baec620de10"
+        ],
+        "attrs": {}
+      },
+      {
+        "type": "NodeLabel",
+        "label": true,
+        "size": {
+          "width": 15,
+          "height": 15
+        },
+        "position": {
+          "x": 222.5,
+          "y": 145
+        },
+        "id": "f70f632c-e54d-4002-9ec5-7baec620de10",
+        "z": 3,
+        "parent": "a4d257fe-72cb-4334-9a1f-b5a8b4f886fb",
+        "attrs": {
+          "label": {
+            "fill": "#C5C8C6",
+            "refX": "50%",
+            "textAnchor": "middle",
+            "refY": "50%",
+            "textVerticalAnchor": "middle"
+          }
+        }
+      },
+      {
+        "type": "State",
+        "position": {
+          "x": 200,
+          "y": 200
+        },
+        "size": {
+          "height": 60,
+          "width": 60
+        },
+        "angle": 0,
+        "fixedRatio": false,
+        "embedable": true,
+        "linkable": true,
+        "id": "36b8548a-74b1-4925-ae23-291c03231ee6",
+        "z": 4,
+        "attrs": {
+          "name": {
+            "text": "StateA"
+          }
+        }
+      },
+      {
+        "type": "State",
+        "position": {
+          "x": 400,
+          "y": 300
+        },
+        "size": {
+          "height": 60,
+          "width": 60
+        },
+        "angle": 0,
+        "fixedRatio": false,
+        "embedable": true,
+        "linkable": true,
+        "id": "db372094-acc6-467b-b44d-495606208445",
+        "z": 5,
+        "attrs": {
+          "name": {
+            "text": "StateB"
+          }
+        }
+      },
+      {
+        "type": "Transition",
+        "source": {
+          "id": "a4d257fe-72cb-4334-9a1f-b5a8b4f886fb"
+        },
+        "target": {
+          "id": "36b8548a-74b1-4925-ae23-291c03231ee6"
+        },
+        "router": {
+          "name": "orthogonal",
+          "args": {
+            "padding": 8
+          }
+        },
+        "connector": {
+          "name": "rounded"
+        },
+        "labels": [
+          {
+            "attrs": {
+              "text": {
+                "fill": "#555555"
+              }
+            },
+            "position": {}
+          },
+          {
+            "attrs": {
+              "label": {
+                "text": "1",
+                "fill": "#555555"
+              },
+              "circleBody": {
+                "fill": "#F0F0F0",
+                "stroke": "#555555"
+              }
+            }
+          }
+        ],
+        "id": "9b5ce41b-0116-46a8-a603-ea4119c6e5d7",
+        "z": 6,
+        "attrs": {}
+      },
+      {
+        "type": "Transition",
+        "source": {
+          "id": "36b8548a-74b1-4925-ae23-291c03231ee6"
+        },
+        "target": {
+          "id": "db372094-acc6-467b-b44d-495606208445"
+        },
+        "router": {
+          "name": "orthogonal",
+          "args": {
+            "padding": 8
+          }
+        },
+        "connector": {
+          "name": "rounded"
+        },
+        "labels": [
+          {
+            "attrs": {
+              "text": {
+                "text": "after 1 s",
+                "fill": "#555555"
+              }
+            },
+            "position": {}
+          },
+          {
+            "attrs": {
+              "label": {
+                "text": "1",
+                "fill": "#555555"
+              },
+              "circleBody": {
+                "fill": "#F0F0F0",
+                "stroke": "#555555"
+              }
+            }
+          }
+        ],
+        "id": "f4fdd1fd-fc69-42a9-a51b-34aeb2fd7fe4",
+        "z": 7,
+        "attrs": {
+          "line": {
+            "strokeWidth": 4
+          }
+        }
+      },
+      {
+        "type": "Transition",
+        "source": {
+          "id": "db372094-acc6-467b-b44d-495606208445"
+        },
+        "target": {
+          "id": "36b8548a-74b1-4925-ae23-291c03231ee6"
+        },
+        "router": {
+          "name": "orthogonal",
+          "args": {
+            "padding": 8
+          }
+        },
+        "connector": {
+          "name": "rounded"
+        },
+        "labels": [
+          {
+            "attrs": {
+              "text": {
+                "text": "myEvent",
+                "fill": "#555555"
+              }
+            },
+            "position": {}
+          },
+          {
+            "attrs": {
+              "label": {
+                "text": "1",
+                "fill": "#555555"
+              },
+              "circleBody": {
+                "fill": "#F0F0F0",
+                "stroke": "#555555"
+              }
+            }
+          }
+        ],
+        "id": "7197094a-c890-4098-9170-1701f27ac3cb",
+        "z": 8,
+        "attrs": {}
+      }
+    ]
+  },
+  "genModel": {
+    "generator": {
+      "schemaKey": "yakindu::c",
+      "LicenseHeader": {
+        "licenseText": ""
+      },
+      "FunctionInlining": {
+        "inlineReactions": false,
+        "inlineEntryActions": false,
+        "inlineExitActions": false,
+        "inlineEnterSequences": false,
+        "inlineExitSequences": false,
+        "inlineChoices": false,
+        "inlineEnterRegion": false,
+        "inlineExitRegion": false,
+        "inlineEntries": false
+      },
+      "OutEventAPI": {
+        "observables": false,
+        "getters": false
+      },
+      "IdentifierSettings": {
+        "moduleName": "MyFirstStatechart",
+        "statemachinePrefix": "myFirstStatechart",
+        "separator": "_",
+        "headerFilenameExtension": "h",
+        "sourceFilenameExtension": "c"
+      },
+      "Tracing": {
+        "enterState": false,
+        "exitState": false,
+        "generic": false
+      },
+      "Includes": {
+        "useRelativePaths": false
+      },
+      "GeneratorOptions": {
+        "userAllocatedQueue": false,
+        "metaSource": false
+      },
+      "GeneralFeatures": {
+        "timerService": false
+      }
+    }
+  }
+}
